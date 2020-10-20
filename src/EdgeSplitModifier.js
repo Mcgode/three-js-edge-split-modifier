c8aaa62ace862e291756e94cfd595107e582527b
Max Godefroy (Clyde) <max@godefroy.net>
2020-10-20 09:16:16 +0200

    EdgeSplitModifier - Switched to using indexed geometries

--- src/EdgeSplitModifier.js
+++ src/EdgeSplitModifier.js
@@ -1,4 +1,4 @@
-import {BufferAttribute, BufferGeometry, Vector3} from "three";
+import {BufferAttribute, BufferGeometry, BufferGeometryUtils, Vector3} from "three";
 
 
 function EdgeSplitModifier()
@@ -9,42 +9,84 @@ function EdgeSplitModifier()
     let D = new Vector3();
 
 
-    this._MapPoints = function ( positions )
+    this._MapPoints = function () {
+
+        for ( let i = 0; i < this._positions.length; i += 3 ) {
+
+            let key = `${this._positions[i]},${this._positions[i+1]},${this._positions[i+2]}`
+
+            if ( !this._pointMap.has( key ) )
+                this._pointMap.set( key, [] )
+
+            this._pointMap.get( key ).push( i / 3 )
+
+        }
+
+    }
+
+
+    this._ComputeNormals = function ()
     {
 
-        for (let i = 0; i < positions.length; i += 3) {
+        this._normals = new Float32Array(this._indexes.length * 3)
 
-            let key = `${positions[i]},${positions[i+1]},${positions[i+2]}`
+        for (let i = 0; i < this._indexes.length; i+= 3) {
 
-            if ( !this._pointMap.has(key) )
-                this._pointMap.set(key, [])
+            let index = this._indexes[i]
+            A.set(
+                this._positions[3*index],
+                this._positions[3*index + 1],
+                this._positions[3*index + 2]);
 
-            this._pointMap.get(key).push(i / 3)
+            index = this._indexes[i+1]
+            B.set(
+                this._positions[3*index],
+                this._positions[3*index + 1],
+                this._positions[3*index + 2]);
+
+            index = this._indexes[i+2]
+            C.set(
+                this._positions[3*index],
+                this._positions[3*index + 1],
+                this._positions[3*index + 2]);
+
+            C.sub(B);
+            A.sub(B);
+
+            let normal = C.cross(A).normalize();
+
+            if (normal.length() === 0)
+                console.log("Error computing normal")
+
+            for (let j = 0; j < 3; j++) {
+                this._normals[3*(i+j)] = normal.x
+                this._normals[3*(i+j)+1] = normal.y
+                this._normals[3*(i+j)+2] = normal.z
+            }
 
         }
 
     }
 
 
-    this._ComputeFaceAreas = function ( positions )
+    this._MapPositionsToIndexes = function ()
     {
 
-        this._areas = new Float32Array(positions.length / 9)
+        this._pointToIndexMap = Array(this._positions.length / 3)
 
-        for (let i = 0; i < positions.length; i+= 9) {
+        for (let i = 0; i < this._indexes.length; i++) {
 
-            A.set(positions[i], positions[i+1], positions[i+2]);
-            B.set(positions[i+3], positions[i+4], positions[i+5]);
-            C.set(positions[i+6], positions[i+7], positions[i+8]);
+            let index = this._indexes[i]
 
-            B.sub(A);
-            C.sub(A);
+            if (this._pointToIndexMap[index] == null)
+                this._pointToIndexMap[index] = [];
 
-            this._areas[i / 9] = B.cross( C ).length() / 2
+            this._pointToIndexMap[index].push(i)
 
         }
 
     }
+
 
     this._EdgeSplitToGroups = function( indexes, cutOff, firstIndex )
     {
@@ -54,27 +96,36 @@ function EdgeSplitModifier()
 
         let result = {
             splitGroup: [],
-            currentGroup: []
+            currentGroup: [firstIndex]
         }
 
         for (let j of indexes) {
 
-            B.set( this._normals[3 * j], this._normals[3 * j + 1], this._normals[3 * j + 2] )
-                .normalize()
+            if (j !== firstIndex) {
 
-            if ( B.dot(A) < cutOff )
-                result.splitGroup.push(j)
+                B.set( this._normals[3 * j], this._normals[3 * j + 1], this._normals[3 * j + 2] )
+                    .normalize()
 
-            else
-                result.currentGroup.push(j)
+                if ( B.dot(A) < cutOff )
+                    result.splitGroup.push(j)
+
+                else
+                    result.currentGroup.push(j)
+
+            }
 
         }
 
         return result
     }
 
-    this._EdgeSplit = function ( indexes, cutOff )
+
+    this._EdgeSplit = function ( indexes, cutOff, original = null )
     {
+
+        if ( indexes.length === 0 )
+            return;
+
         let groupResults = []
         for (let index of indexes)
             groupResults.push(this._EdgeSplitToGroups(indexes, cutOff, index))
@@ -84,25 +135,14 @@ function EdgeSplitModifier()
             if ( groupResult.currentGroup.length > result.currentGroup.length )
                 result = groupResult
 
-        C.set(0, 0, 0)
-        for ( let index of result.currentGroup ) {
-
-            B.set( this._normals[3 * index], this._normals[3 * index + 1], this._normals[3 * index + 2] )
-            C.addScaledVector( B, this._areas[Math.floor( index / 3 )] )
-
-        }
-
-        C.normalize()
-        for ( let index of result.currentGroup ) {
-
-            this._normals[3 * index] = C.x;
-            this._normals[3 * index + 1] = C.y;
-            this._normals[3 * index + 2] = C.z;
-
-        }
+        if ( original != null )
+            this._splitIndexes.push({
+                original: original,
+                indexes: result.currentGroup
+            })
 
         if ( result.splitGroup.length )
-            this._EdgeSplit( result.splitGroup, cutOff )
+            this._EdgeSplit( result.splitGroup, cutOff, original || result.currentGroup[0] )
 
     }
 
@@ -119,23 +159,68 @@ function EdgeSplitModifier()
         }
 
 
-        let positions = geometry.getAttribute( "position" ).array;
-        this._MapPoints( positions );
-        this._ComputeFaceAreas( positions );
+        if ( geometry.index == null )
+            geometry = BufferGeometryUtils.mergeVertices( geometry )
+        // return geometry
 
 
-        this._normals = geometry.getAttribute( "normal" ).array;
-        if ( new Vector3(this._normals[0], this._normals[1], this._normals[2]).length() === 0 )
-            geometry.computeVertexNormals()
-        this._normals = geometry.getAttribute( "normal" ).array;
+        this._indexes = geometry.index.array
+        this._positions = geometry.getAttribute( "position" ).array;
+
+        this._ComputeNormals()
 
 
-        for ( let [_, vertexIndexes] of this._pointMap )
+        this._MapPositionsToIndexes()
+
+
+        this._splitIndexes = []
+
+        for ( let vertexIndexes of this._pointToIndexMap )
             this._EdgeSplit( vertexIndexes, Math.cos(cutOffAngle) - 0.001 )
 
-        geometry.setAttribute("normal", new BufferAttribute(this._normals, 3, true))
 
-       return geometry
+        let newPositions = new Float32Array( this._positions.length + 3 * this._splitIndexes.length )
+        newPositions.set(this._positions)
+        let offset = this._positions.length
+
+        // let newNormals = new Float32Array( this._positions.length + 3 * this._splitIndexes.length )
+        //
+        // for ( let i = 0; i < this._pointToIndexMap.length; i++ ) {
+        //     let index = this._pointToIndexMap[i][0]
+        //     newNormals[3*i]   = this._normals[3*index]
+        //     newNormals[3*i+1] = this._normals[3*index+1]
+        //     newNormals[3*i+2] = this._normals[3*index+2]
+        // }
+
+        let indexes = new Uint32Array( this._indexes.length )
+        indexes.set( this._indexes )
+
+        for ( let i = 0; i < this._splitIndexes.length; i++) {
+
+            let split = this._splitIndexes[i]
+            let index = this._indexes[split.original];
+
+            newPositions[offset + 3*i]   = this._positions[3*index]   //+ this._normals[3 * index];
+            newPositions[offset + 3*i+1] = this._positions[3*index+1] //+ this._normals[3 * index + 1];
+            newPositions[offset + 3*i+2] = this._positions[3*index+2] //+ this._normals[3 * index + 2];
+
+            // newNormals[offset + 3*i]   = this._normals[3*split.original]   //+ this._normals[3 * index];
+            // newNormals[offset + 3*i+1] = this._normals[3*split.original+1] //+ this._normals[3 * index + 1];
+            // newNormals[offset + 3*i+2] = this._normals[3*split.original+2] //+ this._normals[3 * index + 2];
+
+            for ( let j of split.indexes )
+                indexes[j] = offset / 3 + i
+
+        }
+
+        // newPositions.fill(0)
+
+        geometry = new BufferGeometry()
+        geometry.setAttribute('position', new BufferAttribute(newPositions, 3, true))
+        // geometry.setAttribute('normal', new BufferAttribute(newNormals, 3))
+        geometry.setIndex(new BufferAttribute(indexes, 1))
+
+        return geometry
 
     }
 }
